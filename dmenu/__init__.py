72f8b48f8d2779346c92f5bdbe220480e9e882b6
MelianMiko <support@mmk.pw>
2024-01-18 05:27:48 +0700

    Add ability to pass args to callback

--- dmenu/__init__.py
+++ dmenu/__init__.py
@@ -6,17 +6,28 @@ extra_args = []
 
 class DMenuContent:
     def __init__(self, items: list[tuple[str, callable]] = None, extra_args: list[str] = None):
-        self._items = [] if items is None else items            # type: list[tuple[str, callable]]
+        self._items = []
         self._args = [] if extra_args is None else extra_args   # type: list[str]
+
+        if items is not None:
+            for name, fnc in items:
+                self._items.append((name, fnc, []))
+
         self.validate_items()
 
     def item(self, name):
         def _in(fnc):
-            self._items.append((name, fnc))
+            self._items.append((name, fnc, []))
             self.validate_items()
             return fnc
 
         return _in
+
+    def append(self, name, fnc, args=None):
+        if args is None:
+            args = []
+        self._items.append((name, fnc, args))
+        self.validate_items()
 
     def run(self):
         process = subprocess.Popen([executable_path, *extra_args, *self._args],
@@ -28,9 +39,9 @@ class DMenuContent:
         if data_out == "":
             return False
 
-        for name, fnc in self._items:
+        for name, fnc, args in self._items:
             if name == data_out:
-                fnc()
+                fnc(*args)
                 return True
 
         return False
